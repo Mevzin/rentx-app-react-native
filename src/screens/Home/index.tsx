9369c0d851f8fc59c048eb2e1b570d7bd8b101ad
Thiago Torres <thiagomev@gmail.com>
2022-05-03 04:28:02 -0300

    Finished Header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React from "react";
+import { StatusBar } from "react-native";
+import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import {
-  Container,
-  Title
-} from './styles';
+import Logo from "../../assets/logo.svg";
 
 export function Home() {
   return (
     <Container>
-      <Title>
-        Home
-      </Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
