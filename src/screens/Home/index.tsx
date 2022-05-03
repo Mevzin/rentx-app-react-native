d3502585cba21873566ad7f4afba806595b03815
Thiago Torres <thiagomev@gmail.com>
2022-05-03 05:31:03 -0300

    Finished Card

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,7 +5,38 @@ import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
 
+import { Car } from "../../components/Car";
+
 export function Home() {
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent : {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png'
+  }
+
+  const carDataTwo = {
+    brand: 'Porshe',
+    name: 'Panamera',
+    rent : {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://www.mazettoseguros.com.br/blog/wp-content/uploads/2019/10/seguro-panamera-corretora-de-seguros-700x350.png'
+  }
+
+  const carDataThree = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent : {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png'
+  }
   return (
     <Container>
       <StatusBar
@@ -19,6 +50,9 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
+      <Car data={carDataThree} />
     </Container>
   );
 }
