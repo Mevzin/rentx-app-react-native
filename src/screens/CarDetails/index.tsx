3e86fc868dce1921d101e8fb235329a87412b8b3
Thiago Torres <thiagomev@gmail.com>
2022-07-25 00:27:52 -0300

    ✔ Finished Car details screen

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { Accessory } from "../../components/Accessory";
 
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header, CarImages } from "./styles";
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from "./styles";
+import { Button } from "../../components/Button";
 
 export function CarDetails() {
   return (
@@ -16,6 +40,37 @@ export function CarDetails() {
           imagesUrl={["https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png"]}
         />
       </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lanborguini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 200,00</Price>
+          </Rent>
+        </Details>
+        <Accessories>
+          <Accessory name="380km/h" icon={SpeedSvg}/>
+          <Accessory name="3.2s" icon={AccelerationSvg}/>
+          <Accessory name="800 HP" icon={ForceSvg}/>
+          <Accessory name="Gasolina" icon={GasolineSvg}/>
+          <Accessory name="Auto" icon={ExchangeSvg}/>
+          <Accessory name="2 pessoas" icon={PeopleSvg}/>
+        </Accessories>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar"/>
+      </Footer>
+
     </Container>
   );
 }
